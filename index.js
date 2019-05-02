e77e2ec155664fff160af952cfe93aca5505f6e1
Kristian Freeman <kristian@kristianfreeman.com>
2019-05-02 11:37:53 -0500

    Add graphiql and refactor to handlers

--- index.js
+++ index.js
@@ -1,32 +1,13 @@
-const { ApolloServer, gql } = require('apollo-server-cloudflare')
-const { graphqlCloudflare } = require('apollo-server-cloudflare/dist/cloudflareApollo')
-
-const typeDefs = gql`
-  type Query {
-    hello: String
-  }
-`
-
-const resolvers = {
-  Query: {
-    hello: () => 'Hello world!',
-  },
-}
-
-const server = new ApolloServer({
-  typeDefs,
-  introspection: true,
-  resolvers,
-})
+const apollo = require('./src/handlers/apollo')
+const graphiql = require('./src/handlers/graphiql')
 
 const handleRequest = async request => {
   const url = new URL(request.url)
   try {
     if (url.pathname === '/graphql') {
-      return graphqlCloudflare(() => server.createGraphQLServerOptions(request))(request)
+      return apollo(request)
     } else if (url.pathname === '/graphiql') {
-      // graphiql here
-      return new Response('ok')
+      return graphiql(request)
     } else {
       return new Response('Not found', { status: 404 })
     }
