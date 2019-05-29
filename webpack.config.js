1d7b6cecfba40b9bbad02f05e6b7698aff1261bb
Kristian Freeman <kristian@kristianfreeman.com>
2019-05-29 13:40:51 -0500

    Remove null-ed dependencies, they fit now!

--- webpack.config.js
+++ webpack.config.js
@@ -4,8 +4,6 @@ module.exports = {
   target: 'webworker',
   resolve: {
     alias: {
-      'apollo-engine-reporting': path.resolve(__dirname, './null.js'),
-      busboy: path.resolve(__dirname, './null.js'),
       fs: path.resolve(__dirname, './null.js'),
     },
   },
