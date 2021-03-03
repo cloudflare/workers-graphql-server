dbe2bca4233434d64775b7ed18b73fd3a5838943
Jesse Rosenberger <git@jro.cc>
2021-03-03 09:15:58 +0000

    Add comment as to why 'fs' builtin module is depended on.
    Hopefully making it easier to understand why this was here and when it can
    be removed!

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,10 @@ module.exports = {
   target: 'webworker',
   resolve: {
     alias: {
+      // While Apollo Server doesn't use the 'fs' Node.js builtin itself,
+      // its dependency - graphql-upload - does leverage it.
+      // An intention is for Apollo Server 3.x to no longer directly rely on
+      // graphql-upload, so this may be re-visited when that release occurs.
       fs: path.resolve(__dirname, './null.js'),
 
       // The 'net' and 'tls' Node.js built-in usage within Apollo Server
